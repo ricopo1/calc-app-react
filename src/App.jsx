f6a9a69aaea7a2ba77fdf86984dbac5cba1d59fb
ricopo1 <yooutubericoo@gmail.com>
2020-09-26 00:08:03 +0200

    style button result y mejoras funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// Componente básico
-// importación 
 /* eslint no-eval: 0 */ // Ignora el warning de usar eval()
 import React, { useState } from 'react'
 import words from 'lodash.words'
@@ -9,14 +7,13 @@ import Numbers from './components/Numbers'
 import Result from './components/Result'
 import './App.css'
 
-// generación de la función del componente
-// función flecha, sin parámetros "()"
 const App = () => {
     
     const [stack, setStack] = useState("")
-    const items = words(stack, /[^-^+^*^/]+/g)
+    const checkIsNegative = value => Math.sign(value)===-1?true:false
+    const items = words(stack, checkIsNegative(stack)?/[^+^*^/]+/g:/[^-^+^*^/]+/g)
     const value = items.length > 0 ? items[items.length-1] : "0"
-    
+  
     return (
     <main className='react-calculator'>
         <Result value={value}/>
@@ -31,8 +28,23 @@ const App = () => {
             }}
         />
         <MathOperations 
-            onClickOperation={operation => setStack(`${stack}${operation}`)} 
-            onClickEqual={equal => setStack(eval(stack).toString())}
+            onClickOperation={operation => {
+                if(stack.length > 0) {
+                    const operationExist = stack[stack.length-1].match(/[+*/-]/g)
+                    if(operationExist) setStack(`${stack.replace(stack[stack.length-1], operation)}`)
+                    else setStack(`${stack}${operation}`)
+                } else if(operation === "-") {
+                    setStack(`${operation}${stack}`)
+                }
+            }} 
+            onClickEqual={equal => {
+                try {
+                    setStack(eval(stack).toString())
+                } catch (err) {
+                    alert(err)
+                    setStack('')
+                }
+            }}
         />
     </main>)
 }
